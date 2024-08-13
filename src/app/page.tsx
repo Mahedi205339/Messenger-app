76920af5ad85c76dfb16e7973656fa97ea8a4010
Mahedi205339 <mahedi205339@gmail.com>
2024-08-14 01:01:43 +0600

    added user authentication part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import Button from "@/components/ui/Button";
 import { db } from "@/lib/db";
 
 export default async function Home() {
-  
-  await db.set('hello','hello')
+
+  await db.set('hello', 'hello')
 
   return (
     <div>
       <h2>Home</h2>
+      <Button variant={"default"}>Click</Button>
     </div>
   );
 }
