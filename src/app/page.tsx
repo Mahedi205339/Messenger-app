af5991ffa95d4fd0ccb47386272363d72703621f
Mahedi205339 <mahedi205339@gmail.com>
2024-08-13 18:59:11 +0600

    added database (Redis) set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  
+  await db.set('hello','hello')
+
   return (
     <div>
       <h2>Home</h2>
