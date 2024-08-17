3df1462572359ab707b58e1ffca7f0c0f436d5f0
Mahedi205339 <mahedi205339@gmail.com>
2024-08-17 23:59:01 +0600

    added new file convension

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <main className='pt-8'>
+            <h1>Add a friend</h1>
+        </main>
+    );
+};
+
+export default page;
