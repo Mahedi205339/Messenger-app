8bcec555b2d2c6b81ebea654f41c48f8a873cb20
Mahedi205339 <mahedi205339@gmail.com>
2024-08-18 23:37:48 +0600

    added add friend custom form and button

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,10 +1,12 @@
+import AddFriendButton from '@/components/AddFriendButton';
 import React from 'react';
 
 const page = () => {
     return (
         <main className='pt-8'>
             {/* friend request section */}
-            <h1>Add a friend</h1> 
+            <h1 className='font-bold text-5xl mb-8'>Add a friend</h1> 
+            <AddFriendButton/>
         </main>
     );
 };
