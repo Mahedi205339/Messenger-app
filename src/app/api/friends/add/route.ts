f3959a86533c35512d457c1cc9d32a9963c17508
Mahedi205339 <mahedi205339@gmail.com>
2024-08-20 23:59:36 +0600

    added full route

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 "use server"
 
+import { authOptions } from "@/lib/auth";
 import { addFriendValidator } from "@/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
 
 export async function POST(req: Request) {
     try {
@@ -8,9 +10,23 @@ export async function POST(req: Request) {
         const body = await req.json();
 
         const { email: emailToAdd } = addFriendValidator.parse(body.email)
-        const 
-        RESTResponse = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/`)
+        const
+            RESTResponse = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/user:email${emailToAdd}`, {
+                headers: {
+                    Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+                },
+                cache: 'no-store',
+            })
+        const data = await RESTResponse.json() as { result: string }
+        // console.log(data)
 
+        const idToAdd = data.result;
+
+        const session = await getServerSession(authOptions)
+
+        if (!session) {
+            return new Response('Unauthorized ', { status: 401 })
+        }
     }
     catch (error) {
 
