90ba0d62d46f666be038e3d203da5e6ca710e24e
Mahedi205339 <mahedi205339@gmail.com>
2024-08-26 23:38:21 +0600

    added sidebar 1st route

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import { Icons } from '@/components/icons';
+import { Icons,Icon } from '@/components/icons';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
@@ -9,8 +9,22 @@ import React, { ReactNode } from 'react';
 interface LayoutProps {
     children: ReactNode
 }
+interface SidebarOption{
+    id: number
+    name: string 
+    href : string
+    Icon : Icon
+    
+}
 
-//not main layout
+ const sidebarOptions :SidebarOption[] =[
+    {
+        id:1,
+        name: 'Add friend',
+        href:'/dashboard/add',
+        Icon:'UserPlus'
+    }
+ ]
 
 const layout = async ({ children }: LayoutProps) => {
 
@@ -34,6 +48,21 @@ const layout = async ({ children }: LayoutProps) => {
             </li>
             <li>
                 <div className='text-xs font-semibold leading-6 text-gray-400'>Overview</div>
+                <ul role='list' className='-mx-2 space-y-1'>
+                    {sidebarOptions.map((option) =>{
+                        const Icon = Icons[option.Icon]
+                        return (
+                            <li key={option.id}>
+                            <Link href={option.href} className='text-gray-700 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'>
+                            <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
+                                <Icon className='h-4 w-4'/>
+                            </span>
+                            <span className='truncate'>{option.name} </span>
+                            </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
             </li>
         </ul>
         </nav>
