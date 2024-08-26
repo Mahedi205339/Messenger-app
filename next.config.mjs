de9246802b2e6c96f48cb7cd7e7c978e5c5f91ed
Mahedi205339 <mahedi205339@gmail.com>
2024-08-26 23:49:27 +0600

    added some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental:{
+        appDir :true,
+
+    },
+    images:{
+        domains:''
+    }
+};
 
 export default nextConfig;
--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import { Icons,Icon } from '@/components/icons';
+import { Icons, Icon } from '@/components/icons';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
+import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import React, { ReactNode } from 'react';
@@ -9,22 +10,22 @@ import React, { ReactNode } from 'react';
 interface LayoutProps {
     children: ReactNode
 }
-interface SidebarOption{
+interface SidebarOption {
     id: number
-    name: string 
-    href : string
-    Icon : Icon
-    
+    name: string
+    href: string
+    Icon: Icon
+
 }
 
- const sidebarOptions :SidebarOption[] =[
+const sidebarOptions: SidebarOption[] = [
     {
-        id:1,
+        id: 1,
         name: 'Add friend',
-        href:'/dashboard/add',
-        Icon:'UserPlus'
+        href: '/dashboard/add',
+        Icon: 'UserPlus'
     }
- ]
+]
 
 const layout = async ({ children }: LayoutProps) => {
 
@@ -41,31 +42,50 @@ const layout = async ({ children }: LayoutProps) => {
                     Your chats
                 </div>
 
-        <nav className='flex flex-1 flex-col'>
-        <ul role='list' className='flex flex-1 flex-col gap-y-7'>
-            <li>
+                <nav className='flex flex-1 flex-col'>
+                    <ul role='list' className='flex flex-1 flex-col gap-y-7'>
+                        <li>
                 // chats that this user has
-            </li>
-            <li>
-                <div className='text-xs font-semibold leading-6 text-gray-400'>Overview</div>
-                <ul role='list' className='-mx-2 space-y-1'>
-                    {sidebarOptions.map((option) =>{
-                        const Icon = Icons[option.Icon]
-                        return (
-                            <li key={option.id}>
-                            <Link href={option.href} className='text-gray-700 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'>
-                            <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
-                                <Icon className='h-4 w-4'/>
-                            </span>
-                            <span className='truncate'>{option.name} </span>
-                            </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </li>
-        </ul>
-        </nav>
+                        </li>
+                        <li>
+                            <div className='text-xs font-semibold leading-6 text-gray-400'>Overview</div>
+                            <ul role='list' className='-mx-2 space-y-1'>
+                                {sidebarOptions.map((option) => {
+                                    const Icon = Icons[option.Icon]
+                                    return (
+                                        <li key={option.id}>
+                                            <Link href={option.href} className='text-gray-700 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'>
+                                                <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
+                                                    <Icon className='h-4 w-4' />
+                                                </span>
+                                                <span className='truncate'>{option.name} </span>
+                                            </Link>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </li>
+                        <li className='-mx-6 flex items-center'>
+                            <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
+                                <div className='relative h-8 w-8 bg-gray-50'>
+                                    <Image
+                                        fill
+                                        referrerPolicy='no-referrer'
+                                        className='rounded-full'
+                                        src={session.user.image}
+                                        alt='your profile picture'
+                                    />
+                                </div>
+                                <span className='sr-only'>Your profile</span>
+                                <div className='flex flex-col'>
+                                    <span aria-hidden='true'>{session.user.name} </span>
+                                    <span aria-hidden className='text-xs text-zinc-400' >{session.user.email} </span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+                </nav>
 
             </div>
             {children}
