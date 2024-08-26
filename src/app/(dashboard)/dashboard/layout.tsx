c18c67312302328a1535418d44c432ba62bcb1d8
Mahedi205339 <mahedi205339@gmail.com>
2024-08-26 22:58:30 +0600

    added logout routing

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 import React, { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -11,9 +13,12 @@ interface LayoutProps {
 const layout = async ({ children }: LayoutProps) => {
 
     const session = await getServerSession(authOptions)
+    if(!session) notFound()
 
     return (
-        <div>
+        <div className='w-full flex h-screen'>
+            <div className='flex h-full w-full max-w-xs flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white p-6'></div>
+            <Link href='/dashboard'></Link>
             {children}
         </div>
     );
