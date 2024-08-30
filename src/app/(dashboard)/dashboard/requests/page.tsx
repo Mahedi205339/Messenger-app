efbfec9f1d0e7a24a6ebd396bfd3c2dd77838655
Mahedi205339 <mahedi205339@gmail.com>
2024-08-30 22:54:54 +0600

    added friend req managing functions

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,35 @@
+import { fetchRedis } from '@/helpers/redis';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+const page = async () => {
+    const session = await getServerSession(authOptions)
+    if (!session) notFound()
+
+    const incomingSenderIds = (
+        await fetchRedis(
+            'smembers',
+            `user:${session.user.id}:incoming_friend_request`
+        ) as string[]
+    )
+
+    const incomingFriendRequest = await Promise.all(
+        incomingSenderIds.map(async (senderId) =>{
+            const sender = await fetchRedis('get',`user:${senderId}`)as User
+            return{
+                senderId,
+                senderEmail:sender.email
+            }
+        })
+    )
+
+    return (
+        <div>
+            page
+        </div>
+    );
+};
+
+export default page;
